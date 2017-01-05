6c3a2a284e00b96f8374808dd328e474ccf66bd5
Maximilian Bode <maximilian.bode@tngtech.com>
2017-01-05 08:31:39 +0100

    Add spotless

--- qb-job/src/main/java/com/tngtech/qb/QueryableBillingJob.java
+++ qb-job/src/main/java/com/tngtech/qb/QueryableBillingJob.java
@@ -4,11 +4,11 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 class QueryableBillingJob {
 
-    private final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+  private final StreamExecutionEnvironment env =
+      StreamExecutionEnvironment.getExecutionEnvironment();
 
-    void run() throws Exception {
-        env.fromElements(1, 2, 3)
-           .print();
-        env.execute();
-    }
+  void run() throws Exception {
+    env.fromElements(1, 2, 3).print();
+    env.execute();
+  }
 }
--- qb-job/src/test/java/com/tngtech/qb/QueryableBillingJobTest.java
+++ qb-job/src/test/java/com/tngtech/qb/QueryableBillingJobTest.java
@@ -4,9 +4,9 @@ import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
 import org.junit.Test;
 
 public class QueryableBillingJobTest extends StreamingMultipleProgramsTestBase {
-    @Test
-    public void jobRuns() throws Exception {
-        final QueryableBillingJob job = new QueryableBillingJob();
-        job.run();
-    }
+  @Test
+  public void jobRuns() throws Exception {
+    final QueryableBillingJob job = new QueryableBillingJob();
+    job.run();
+  }
 }
