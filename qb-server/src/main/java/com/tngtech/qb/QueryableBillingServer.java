829a81b745799f7df4c46bd9629431dae0fb8912
Maximilian Bode <maximilian.bode@tngtech.com>
2017-01-05 14:22:34 +0100

    Create skeleton for server that handles queries

---
+++ qb-server/src/main/java/com/tngtech/qb/QueryableBillingServer.java
@@ -0,0 +1,11 @@
+package com.tngtech.qb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryableBillingServer {
+  public static void main(String[] args) {
+    SpringApplication.run(QueryableBillingServer.class, args);
+  }
+}
---
+++ qb-server/src/test/java/com/tngtech/qb/QueryableBillingServerIT.java
@@ -0,0 +1,22 @@
+package com.tngtech.qb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class QueryableBillingServerIT {
+  @Autowired private WebApplicationContext context;
+
+  @Test
+  public void applicationBootsWithoutErrors() {
+    assertThat(context, notNullValue());
+  }
+}
