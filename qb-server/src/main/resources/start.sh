4d286e7bf4320ccee93d8f36dfd1ba6d7165ff80
Konstantin Knauf <konstantin.knauf@tngtech.com>
2017-04-04 21:40:24 +0200

    Increased timeout before starting qb-server

--- qb-server/src/main/resources/start.sh
+++ qb-server/src/main/resources/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sleep 5
+sleep 15
 java -jar -Dspring.profiles.active=flink qb-server-0.1-SNAPSHOT.jar --flink.configDir=/opt/flink/conf --flink.jobIdHex=$(flink list | grep 'Queryable Billing Job' | cut -d ':' -f4 | awk '{$1=$1};1')
